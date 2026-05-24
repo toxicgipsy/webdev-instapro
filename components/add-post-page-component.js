aad86625e822d2a328f6f59140e593839e06bae8
Aman Arazmedov <aman@example.com>
2026-05-24 22:18:58 +0300

    instapro

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -1,15 +1,41 @@
+import { renderHeaderComponent } from "./header-component.js";
+
 export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
   const render = () => {
     // @TODO: Реализовать страницу добавления поста
     const appHtml = `
     <div class="page-container">
       <div class="header-container"></div>
-      Cтраница добавления поста
-      <button class="button" id="add-button">Добавить</button>
+        <div class="form">
+          <h3 class="form-title">Добавить пост</h3>
+            <div class="form-inputs">
+              <div class="upload-image-container">
+                <div class="upload=image">
+                  <label class="file-upload-label secondary-button">
+                    <input
+                      type="file"
+                      class="file-upload-input"
+                      style="display: none"
+                    />
+                    Выберите фото
+                  </label>
+                </div>
+              </div>
+              <label>
+                Опишите фотографию:
+                <textarea class="input textarea" rows="4"></textarea>
+              </label>
+              <button class="button" id="add-button">Добавить</button>
+            </div>
+      </div>
     </div>
   `;
 
     appEl.innerHTML = appHtml;
+
+    renderHeaderComponent({
+      element: document.querySelector(".header-container"),
+    });
 
     document.getElementById("add-button").addEventListener("click", () => {
       onAddPostClick({
